c9f8cec6919b97f9986ffe94495e63c42625be8e
Michael Earls <mearls@hotmail.com>
2016-08-01 15:45:17 -0500

    replaced tabs with 4 spaces

--- LoremIpsumLib/LoremIpsumUtil.cs
+++ LoremIpsumLib/LoremIpsumUtil.cs
@@ -8,43 +8,43 @@ using System.Threading.Tasks;
 
 namespace LoremIpsum
 {
-	public class LoremIpsumFactory
+    public class LoremIpsumFactory
     {
-		public static readonly string APP_KEY_LIPSUM_JSON_API_ENDPOINT = "lipsumJsonApiEndpoint";
+        public static readonly string APP_KEY_LIPSUM_JSON_API_ENDPOINT = "lipsumJsonApiEndpoint";
 
-		public LoremIpsumFactory() { }
+        public LoremIpsumFactory() { }
 
-		public string ApiKey { get; set; }
-		
-		/// <summary>
-		/// The full Url to the JSON API for Lipsum.com.
-		/// </summary>
-		private string LipsumJsonEndpoint
-		{
-			get
-			{
-				if (this.ApiKey != null)
-				{
-					return string.Format(ConfigurationManager.AppSettings[APP_KEY_LIPSUM_JSON_API_ENDPOINT], this.ApiKey);
-				}
-				else
-				{
-					throw new InvalidDataException("The ApiKey property cannot be null");
-				}
-			}
-		}
-
-		/// <summary>
-		/// Gets a new Lorem Ipsum result object from the JSON endpoint at http://lipsum.com.
-		/// </summary>
-		/// <param name="lipsumType">The type of result to get from the api. See <see cref="LipsumType"/></param>
-		/// <param name="amount">An integer value representing how many of the given type to receive from the service.</param>
-		/// <param name="startWithLoremIpsum">true to start with the "Lorem Ipsum" text or false to get a random result.</param>
-		/// <param name="proxyCredentials">Use <see cref="System.Net.NetworkCredential"/>, <see cref="System.Net.CredentialCache.DefaultCredentials"/>, or <see cref="System.Net.CredentialCache.DefaultNetworkCredentials"/> to send to your proxy server.</param>
-		/// <returns>An instance of the <see cref="LoremIpsum"/> class representing the data that was returned from the api.</returns>
-		public LoremIpsum Create(LipsumType lipsumType, int amount, bool startWithLoremIpsum = true, ICredentials proxyCredentials = null)
+        public string ApiKey { get; set; }
+        
+        /// <summary>
+        /// The full Url to the JSON API for Lipsum.com.
+        /// </summary>
+        private string LipsumJsonEndpoint
         {
-			// build the JSON request URL
+            get
+            {
+                if (this.ApiKey != null)
+                {
+                    return string.Format(ConfigurationManager.AppSettings[APP_KEY_LIPSUM_JSON_API_ENDPOINT], this.ApiKey);
+                }
+                else
+                {
+                    throw new InvalidDataException("The ApiKey property cannot be null");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets a new Lorem Ipsum result object from the JSON endpoint at http://lipsum.com.
+        /// </summary>
+        /// <param name="lipsumType">The type of result to get from the api. See <see cref="LipsumType"/></param>
+        /// <param name="amount">An integer value representing how many of the given type to receive from the service.</param>
+        /// <param name="startWithLoremIpsum">true to start with the "Lorem Ipsum" text or false to get a random result.</param>
+        /// <param name="proxyCredentials">Use <see cref="System.Net.NetworkCredential"/>, <see cref="System.Net.CredentialCache.DefaultCredentials"/>, or <see cref="System.Net.CredentialCache.DefaultNetworkCredentials"/> to send to your proxy server.</param>
+        /// <returns>An instance of the <see cref="LoremIpsum"/> class representing the data that was returned from the api.</returns>
+        public LoremIpsum Create(LipsumType lipsumType, int amount, bool startWithLoremIpsum = true, ICredentials proxyCredentials = null)
+        {
+            // build the JSON request URL
             string requestUrl = string.Format(this.LipsumJsonEndpoint, amount, startWithLoremIpsum ? "yes" : "no", lipsumType.GetDescription());
 
             HttpWebRequest request = WebRequest.Create(requestUrl) as HttpWebRequest;
@@ -53,10 +53,10 @@ namespace LoremIpsum
             {
                 request.Proxy.Credentials = proxyCredentials;
             }
-			else 
-			{
-				request.Proxy.Credentials = CredentialCache.DefaultCredentials;
-			}
+            else 
+            {
+                request.Proxy.Credentials = CredentialCache.DefaultCredentials;
+            }
 
             using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
             {
@@ -95,18 +95,18 @@ namespace LoremIpsum
         }
     }
 
-	/// <summary>
-	/// The type of result to retrieve from the JSON endpoint.
-	/// </summary>
-	public enum LipsumType
-	{
-		[Description("words")]
-		Words,
-		[Description("paras")]
-		Paragraphs,
-		[Description("bytes")]
-		Bytes,
-		[Description("lists")]
-		Lists
-	}
+    /// <summary>
+    /// The type of result to retrieve from the JSON endpoint.
+    /// </summary>
+    public enum LipsumType
+    {
+        [Description("words")]
+        Words,
+        [Description("paras")]
+        Paragraphs,
+        [Description("bytes")]
+        Bytes,
+        [Description("lists")]
+        Lists
+    }
 }
--- LoremIpsumSampleApp/Program.cs
+++ LoremIpsumSampleApp/Program.cs
@@ -4,28 +4,28 @@ using System.Net;
 
 namespace LoremIpsum
 {
-	class Program
+    class Program
     {
         static void Main(string[] args)
         {
-			// create our factory
-			var loremIpsum = new LoremIpsumFactory()
-			{
-				ApiKey = ConfigurationManager.AppSettings["lipsumApiKey"]
-			};
+            // create our factory
+            var loremIpsum = new LoremIpsumFactory()
+            {
+                ApiKey = ConfigurationManager.AppSettings["lipsumApiKey"]
+            };
 
-			// the simplest example (get 5 words)
-			var simplestExample = loremIpsum.Create(LipsumType.Words, 5);
-			Debug.WriteLine(simplestExample.Feed.Lipsum);
+            // the simplest example (get 5 words)
+            var simplestExample = loremIpsum.Create(LipsumType.Words, 5);
+            Debug.WriteLine(simplestExample.Feed.Lipsum);
 
-			// a simple example (get 2 paragraphs and don't start with Lorem Ipsum)
-			var simpleExample = loremIpsum.Create(LipsumType.Paragraphs, 2, false);
-			Debug.WriteLine(simpleExample.Feed.Lipsum);
+            // a simple example (get 2 paragraphs and don't start with Lorem Ipsum)
+            var simpleExample = loremIpsum.Create(LipsumType.Paragraphs, 2, false);
+            Debug.WriteLine(simpleExample.Feed.Lipsum);
 
-			// an example of using custom Proxy authentication to get 3 words starting with "Lorem Ipsum"
-			var proxyCredentials = new NetworkCredential("username", "password");
-			var customProxyAuth = loremIpsum.Create(LipsumType.Words, 3, true, proxyCredentials);
-			Debug.WriteLine(customProxyAuth.Feed.Lipsum);
+            // an example of using custom Proxy authentication to get 3 words starting with "Lorem Ipsum"
+            var proxyCredentials = new NetworkCredential("username", "password");
+            var customProxyAuth = loremIpsum.Create(LipsumType.Words, 3, true, proxyCredentials);
+            Debug.WriteLine(customProxyAuth.Feed.Lipsum);
         }
     }
 }
