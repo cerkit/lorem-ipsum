92728b6cbe4d515c88f34df8254cd7aa53a22595
Michael Earls <mearls@hotmail.com>
2016-08-01 14:21:32 -0500

    Updated program.cs
    added GitHub to comment about why I chose to use .Equals(0) instead of ==

--- LoremIpsumSampleApp/Program.cs
+++ LoremIpsumSampleApp/Program.cs
@@ -12,7 +12,7 @@ namespace LoremIpsum
             for (int i = 0; i < 10; i++)
             {
                 // toggle between words and paragraphs
-                bool isEven = (i % 2).Equals(0); // used this because markdown had trouble with double equal signs
+                bool isEven = (i % 2).Equals(0); // used this because GitHub markdown had trouble with double equal signs
                 bool isThird = (i % 3).Equals(0);
                 LipsumType lipsumType = isEven ? LipsumType.Paragraphs : LipsumType.Words;
 
